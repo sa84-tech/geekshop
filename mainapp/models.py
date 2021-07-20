1676036791713d32a363d89fa5b7844752872023
Андрей Громов <D:\Program Files (x86)\The Bat!\Mail>
2021-07-20 17:30:51 +0300

    models added

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,3 +1,79 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(
+        verbose_name='имя',
+        max_length=64,
+        unique=True
+    )
+    description = models.TextField(
+        verbose_name='описание',
+        blank=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        ProductCategory,
+        on_delete=models.PROTECT,
+        verbose_name='категория',
+    )
+    name = models.CharField(
+        verbose_name='имя',
+        max_length=128,
+    )
+    image = models.ImageField(
+        upload_to='products_images',
+        blank=True,
+    )
+    short_desc = models.CharField(
+        max_length=256,
+        blank=True,
+        verbose_name='краткое описание',
+    )
+    description = models.TextField(
+        blank=True,
+        verbose_name='описание',
+    )
+    price = models.DecimalField(
+        verbose_name='цена',
+        max_digits=8,
+        decimal_places=2,
+        default=0,
+    )
+    quantity = models.PositiveIntegerField(
+        verbose_name='количество на складе',
+        default=0,
+    )
+
+from django.db import models
+
+
+class Contact(models.Model):
+    location = models.CharField(
+        verbose_name='населенный пункт',
+        max_length=64,
+    )
+    phone = models.CharField(
+        verbose_name='телефон',
+        max_length=16,
+        blank=True,
+    )
+    email = models.CharField(
+        verbose_name='электронная почта',
+        max_length=64,
+        blank=True,
+    )
+    address = models.CharField(
+        verbose_name='адрес',
+        max_length=256,
+        blank=True,
+    )
