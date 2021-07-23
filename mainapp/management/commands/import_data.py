0013f877d4eb100dbe2a5035fc938e489536346c
sa84 <sa84@list.ru>
2021-07-24 01:23:49 +0300

    user model created

--- mainapp/management/commands/import_data.py
+++ mainapp/management/commands/import_data.py
@@ -3,12 +3,13 @@ import os
 
 from django.core.management import BaseCommand
 
+from authapp.models import ShopUser
 from geekshop.settings import BASE_DIR
 from mainapp.models import Product, ProductCategory, Contact
 
 
 class Command(BaseCommand):
-    def import_data(self):
+    def handle(self, *args, **options):
         try:
             file_path = os.path.join(BASE_DIR, 'mainapp', 'data/init.json')
 
@@ -18,10 +19,7 @@ class Command(BaseCommand):
             if data['categories']:
                 for category in data['categories']:
 
-                    new_category, create = ProductCategory.objects.get_or_create(
-                        name=category['name'],
-                        description=category['description']
-                    )
+                    new_category, create = ProductCategory.objects.get_or_create(**category)
                     if create:
                         new_category.save()
                         print(f'** Category created ** {new_category}')
@@ -33,16 +31,8 @@ class Command(BaseCommand):
                     category, create = ProductCategory.objects.get_or_create(name=product['category'])
                     if create:
                         category.save()
-
-                    new_product, create = Product.objects.get_or_create(
-                        category=category,
-                        name=product['name'],
-                        image=product['image'],
-                        short_desc=product['short_desc'],
-                        description=product['description'],
-                        price=product['price'],
-                        quantity=product['quantity'],
-                    )
+                    product['category'] = category
+                    new_product, create = Product.objects.get_or_create(**product)
 
                     if create:
                         new_product.save()
@@ -53,24 +43,16 @@ class Command(BaseCommand):
             if data['contacts']:
                 for contact in data['contacts']:
 
-                    new_contact, create = Contact.objects.get_or_create(
-                        location=contact['location'],
-                        phone=contact['phone'],
-                        email=contact['email'],
-                        address=contact['address'],
-                    )
+                    new_contact, create = Contact.objects.get_or_create(**contact)
                     if create:
                         new_contact.save()
                         print(f'** Contact created ** {new_contact}')
                     else:
                         print(f'** Contact already exists ** {new_contact}')
 
+            super_user = ShopUser.objects.create_superuser('admin', 'admin@example.com', 'admin', age=36)
+            print(f'** user created ** {super_user}')
+
         except IOError:
             print(str(IOError))
             return
-
-    def handle(self, *args, **options):
-        """
-        Call the function to import data
-        """
-        self.import_data()
