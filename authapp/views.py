73266f84568ff37749246e06f8ce134854338198
sa84 <sa84@list.ru>
2021-07-26 02:37:07 +0300

    register and edit forms are added

--- authapp/views.py
+++ authapp/views.py
@@ -3,22 +3,25 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 
 
 def login(request):
     title = 'вход'
     credentials = {}
 
-    login_form = ShopUserLoginForm(data=request.POST)
-    if request.method == 'POST' and login_form.is_valid():
-        credentials['username'] = request.POST['username']
-        credentials['password'] = request.POST['password']
+    if request.method == 'POST':
+        login_form = ShopUserLoginForm(data=request.POST)
+        if login_form.is_valid():
+            credentials['username'] = request.POST['username']
+            credentials['password'] = request.POST['password']
 
-        user = auth.authenticate(**credentials)
-        if user and user.is_active:
-            auth.login(request, user)
-            return HttpResponseRedirect(reverse('index'))
+            user = auth.authenticate(**credentials)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        login_form = ShopUserLoginForm()
 
     context = {
         'title': title,
@@ -34,8 +37,38 @@ def logout(request):
 
 
 def register(request):
-    pass
+    title = 'регистрация'
+
+    if request.method == 'POST':
+        register_form = ShopUserRegisterForm(request.POST, request.FILES)
+        print('******************', register_form.is_valid())
+        if register_form.is_valid():
+            register_form.save()
+            return HttpResponseRedirect(reverse('auth:login'))
+    else:
+        register_form = ShopUserRegisterForm()
+
+    context = {
+        'title': title,
+        'register_form': register_form
+    }
+    return render(request, 'authapp/register.html', context)
 
 
 def edit(request):
-    pass
+    title = 'редактирование'
+
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    context = {
+        'title': title,
+        'edit_form': edit_form,
+    }
+
+    return render(request, 'authapp/edit.html', context)
