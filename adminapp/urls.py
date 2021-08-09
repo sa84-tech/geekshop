3e340ae4f7139126c103bf701371ea185b5c9586
sa84 <sa84@list.ru>
2021-08-09 22:46:13 +0300

    categories page added to admin panel

--- adminapp/urls.py
+++ adminapp/urls.py
@@ -5,19 +5,21 @@ import adminapp.views as adminapp
 app_name = 'adminapp'
 
 urlpatterns = [
-    # path('users/create/', adminapp.user_create, name='user_create'),
+    path('users/create/', adminapp.user_create, name='user_create'),
     path('users/read/', adminapp.users, name='users'),
-    # path('users/update/<int:pk>'),
-    # path('users/delete/<int:pk>'),
-    #
-    # path('categories/create/'),
-    # path('categories/read/', categories, name=categories),
-    # path('categories/'),
-    # path('categories/'),
-    #
-    # path('products/create/category/<int:pk/'),
-    # path('products/read/catogory/<int:pk/'),
-    # path('products/read/<int:pk>/'),
-    # path('products/update/<int:pk>/'),
-    # path('products/delete/<int:pk>/'),
+    path('users/update/<int:pk>/', adminapp.user_update, name='user_update'),
+    path('users/delete/<int:pk>/', adminapp.user_delete, name='user_delete'),
+    path('users/restore/<int:pk>/', adminapp.user_restore, name='user_restore'),
+
+    path('categories/create/', adminapp.category_create, name='category_create'),
+    path('categories/read/', adminapp.categories, name='categories'),
+    path('categories/update/<int:pk>/', adminapp.category_update, name='category_update'),
+    path('categories/delete/<int:pk>/', adminapp.category_delete, name='category_delete'),
+    path('categories/restore/<int:pk>/', adminapp.category_restore, name='category_restore'),
+
+    path('products/create/category/<int:pk>/', adminapp.product_create, name='product_create'),
+    path('products/read/category/<int:pk>/', adminapp.products, name='products'),
+    path('products/read/<int:pk>/', adminapp.product_read, name='product_read'),
+    path('products/update/<int:pk>/', adminapp.product_update, name='product_update'),
+    path('products/delete/<int:pk>/', adminapp.product_delete, name='product_delete'),
 ]
--- adminapp/views.py
+++ adminapp/views.py
@@ -1,8 +1,36 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+from django.contrib.auth.decorators import user_passes_test
 
+from adminapp.forms import ProductCategoryEditForm
+from authapp.forms import ShopUserRegisterForm, ShopUserEditForm
 from authapp.models import ShopUser
+from mainapp.models import ProductCategory, Product
 
 
+@user_passes_test(lambda u: u.is_superuser)
+def user_create(request):
+    title = 'Пользователи/создание'
+
+    if request.method == 'POST':
+        user_form = ShopUserRegisterForm(request.POST, request.FILES)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin_staff:users'))
+
+    else:
+        user_form = ShopUserRegisterForm()
+
+    context = {
+        'title': title,
+        'user_form': user_form,
+    }
+
+    return render(request, 'adminapp/user_update.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
 def users(request):
     title = 'Панель управления/пользователи'
 
@@ -14,3 +42,178 @@ def users(request):
     }
 
     return render(request, 'adminapp/users.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_update(request, pk):
+    title = 'Пользователи/редактирование'
+
+    cur_user = get_object_or_404(ShopUser, pk=pk)
+    if request.method == 'POST':
+        user_form = ShopUserEditForm(request.POST, request.FILES, instance=cur_user)
+        if user_form.is_valid():
+            user_form.save()
+            return HttpResponseRedirect(reverse('admin_staff:users'))
+
+    else:
+        user_form = ShopUserEditForm(instance=cur_user)
+
+    context = {
+        'title': title,
+        'user_form': user_form,
+    }
+
+    return render(request, 'adminapp/user_update.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_delete(request, pk):
+    title = 'пользователи/удаление'
+
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    print(request.method, user)
+
+    if request.method == 'POST':
+        user.is_active = False
+        user.save()
+        return HttpResponseRedirect(reverse('admin_staff:users'))
+
+    context = {
+        'title': title,
+        'user': user,
+    }
+
+    return render(request, 'adminapp/user_delete.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def user_restore(request, pk):
+    user = get_object_or_404(ShopUser, pk=pk)
+
+    user.is_active = True
+    user.save()
+    return HttpResponseRedirect(reverse('admin_staff:users'))
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def category_create(request):
+    title = 'Категории/создание'
+
+    if request.method == 'POST':
+        category_form = ProductCategoryEditForm(request.POST, request.FILES)
+        if category_form.is_valid():
+            category_form.save()
+            return HttpResponseRedirect(reverse('admin_staff:categories'))
+
+    else:
+        category_form = ProductCategoryEditForm()
+
+    context = {
+        'title': title,
+        'category_form': category_form,
+    }
+
+    return render(request, 'adminapp/category_update.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def categories(request):
+    title = 'Панель управления/категории'
+
+    categories_list = ProductCategory.objects.all()
+
+    print('***********', list(categories_list.values()))
+
+    context = {
+        'title': title,
+        'objects': categories_list,
+    }
+
+    return render(request, 'adminapp/categories.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def category_update(request, pk):
+    title = 'Категории/редактирование'
+
+    cur_category = get_object_or_404(ProductCategory, pk=pk)
+    if request.method == 'POST':
+        category_form = ProductCategoryEditForm(request.POST, instance=cur_category)
+        if category_form.is_valid():
+            category_form.save()
+            return HttpResponseRedirect(reverse('admin_staff:categories'))
+
+    else:
+        category_form = ProductCategoryEditForm(instance=cur_category)
+
+    context = {
+        'title': title,
+        'category_form': category_form,
+    }
+
+    return render(request, 'adminapp/category_update.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def category_delete(request, pk):
+    title = 'Категории / удаление'
+
+    category = get_object_or_404(ProductCategory, pk=pk)
+
+    if request.method == 'POST':
+        category.is_active = False
+        category.save()
+        return HttpResponseRedirect(reverse('admin_staff:categories'))
+
+    context = {
+        'title': title,
+        'category': category,
+    }
+
+    return render(request, 'adminapp/category_delete.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def category_restore(request, pk):
+    category = get_object_or_404(ProductCategory, pk=pk)
+
+    category.is_active = True
+    category.save()
+    return HttpResponseRedirect(reverse('admin_staff:categories'))
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def product_create(request):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def products(request, pk):
+    title = 'Панель администрирование/продукт'
+
+    category = get_object_or_404(ProductCategory, pk=pk)
+    products_list = Product.objects.filter(category__pk=pk).order_by('name')
+
+    context = {
+        'title': title,
+        'category': category,
+        'objects': products_list,
+    }
+
+    return render(request, 'adminapp/products.html', context)
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def product_read(request, pk):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def product_update(request, pk):
+    pass
+
+
+@user_passes_test(lambda u: u.is_superuser)
+def product_delete(request, pk):
+    pass
