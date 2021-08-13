30608bac91e2d9e7c17c2251a28cb3d1437fa9fb
sa84 <sa84@list.ru>
2021-08-13 18:41:24 +0300

    context processor added. minor bugs fixed

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,16 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-from cartapp.models import Cart
-from geekshop.views import get_count, get_total
 from .models import Product, ProductCategory
 
 
 def product_list(request, pk=0, page=1):
     title = 'каталог'
     menu_items = ProductCategory.objects.filter(is_active=True)
-    product_count = get_count(request.user)
-    total_cost = get_total(request.user)
 
     if pk == 0:
         category = {
@@ -35,8 +31,6 @@ def product_list(request, pk=0, page=1):
         'menu_items': menu_items,
         'products': products_paginator,
         'category': category,
-        'count': product_count,
-        'total_cost': total_cost,
     }
 
     return render(request, 'mainapp/product_list.html', context)
@@ -46,9 +40,6 @@ def product_details(request, pk=None):
     print(pk)
     title = 'каталог'
     menu_items = ProductCategory.objects.all()
-
-    product_count = get_count(request.user)
-    total_cost = get_total(request.user)
 
     if pk:
         product = get_object_or_404(Product, pk=pk)
@@ -61,9 +52,6 @@ def product_details(request, pk=None):
         'menu_items': menu_items,
         'products': products,
         'product': product,
-        'count': product_count,
-        'total_cost': total_cost,
-
     }
 
     return render(request, 'mainapp/product.html', context)
