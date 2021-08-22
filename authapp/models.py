db91b1d666e4fba49630203357a9cd0e9e779787
sa84 <sa84@list.ru>
2021-08-22 23:46:56 +0300

    user profile added

--- authapp/models.py
+++ authapp/models.py
@@ -4,6 +4,9 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.timezone import now
 
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
 
 class ShopUser(AbstractUser):
     avatar = models.ImageField(upload_to='users_avatar', blank=True)
@@ -21,3 +24,31 @@ class ShopUser(AbstractUser):
     @property
     def is_activation_key_expired(self):
         return now() > self.activation_key_expires
+
+
+class ShopUserProfile(models.Model):
+    MALE = 'M'
+    FEMALE = 'W'
+
+    GENDER_CHOICES = (
+        (MALE, 'М'),
+        (FEMALE, 'Ж')
+    )
+
+    user = models.OneToOneField(ShopUser, unique=True, null=False, db_index=True, on_delete=models.CASCADE)
+
+    tagline = models.CharField(verbose_name='тэги', max_length=128, blank=True)
+
+    about_me = models.TextField(verbose_name='о себе', max_length=512, blank=True)
+
+    gender = models.CharField(verbose_name='пол', max_length=1, choices=GENDER_CHOICES, blank=True)
+
+    @receiver(post_save, sender=ShopUser)
+    def create_user_profile(sender, instance, created, **kwargs):
+        if created:
+            ShopUserProfile.objects.create(user=instance)
+
+    @receiver(post_save, sender=ShopUser)
+    def save_user_profile(sender, instance, **kwargs):
+        instance.shopuserprofile.save()
+
--- authapp/views.py
+++ authapp/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
+from authapp.forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm, ShopUserProfileEditForm
 from authapp.models import ShopUser
 from geekshop import settings
 
@@ -115,15 +115,19 @@ def edit(request):
 
     if request.method == 'POST':
         edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
-        if edit_form.is_valid():
+        profile_form = ShopUserProfileEditForm(request.POST, instance=request.user.shopuserprofile)
+        if edit_form.is_valid() and profile_form.is_valid():
             edit_form.save()
             return HttpResponseRedirect(reverse('auth:edit'))
     else:
         edit_form = ShopUserEditForm(instance=request.user)
+        profile_form = ShopUserProfileEditForm(instance=request.user.shopuserprofile)
+
 
     context = {
         'title': title,
         'edit_form': edit_form,
+        'profile_form': profile_form,
     }
 
     return render(request, 'authapp/edit.html', context)
