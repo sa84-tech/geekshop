99c282d9eff6eea75b823234283ceea6dc3c75aa
Андрей Громов <D:\Program Files (x86)\The Bat!\Mail>
2021-08-24 18:46:48 +0300

    models and forms for ordersapp added

---
+++ ordersapp/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+from ordersapp.models import Order
+
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
--- ordersapp/views.py
+++ ordersapp/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from ordersapp.models import Order
+
+
+class OrderList(ListView):
+    model = Order
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
